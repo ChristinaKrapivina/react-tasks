c5b06b612ad5d3e316d0a9301b49904ccec0d46c
Kristina Krapyvina <kristina_krapyvina@epam.com>
2020-11-24 22:29:31 +0200

    Updates

---
+++ precache-manifest.5186352e50f03523c7047fc7751681c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "823459d0e5a027cabbe527d946995e76",
+    "url": "/react-tasks/index.html"
+  },
+  {
+    "revision": "ad88c2897b382ab6ab5a",
+    "url": "/react-tasks/static/css/main.f96af41d.chunk.css"
+  },
+  {
+    "revision": "7f78ed114d96144019c7",
+    "url": "/react-tasks/static/js/2.b2161fb0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-tasks/static/js/2.b2161fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad88c2897b382ab6ab5a",
+    "url": "/react-tasks/static/js/main.4c60af2d.chunk.js"
+  },
+  {
+    "revision": "ff87ad0bd5b1829b8958",
+    "url": "/react-tasks/static/js/runtime-main.a2b99fdd.js"
+  }
+]);
